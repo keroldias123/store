cfaed3c88df5c6c51125b684653db2af0b8b2f3b
Keroldias123 <keroldias20@gmail.com>
2026-01-10 13:14:02 +0000

    model organization

---
+++ packages/auth/models/organization.ts
@@ -0,0 +1,11 @@
+// organização (organização)
+
+import { z } from 'zod'
+
+export const organizationSchema = z.object({
+  __typename: z.literal('Organization').default('Organization'),
+  id: z.string(),
+  ownerId: z.string(),
+})
+
+export type Organization = z.infer<typeof organizationSchema>
