68472e2ca3d43f10bc8b5627337ff65c0e4932bb
Keroldias123 <keroldias20@gmail.com>
2026-01-10 12:35:55 +0000

    role user

---
+++ packages/auth/role.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod'
+
+export const roleSchema = z.union([
+  z.literal('ADMIN'),
+  z.literal('MEMBER'),
+  z.literal('CLIENT'),
+  z.literal('BILLING'),
+])
+
+export type Role = z.infer<typeof roleSchema>
