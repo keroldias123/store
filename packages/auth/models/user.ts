efb8ca8c101908e56f71c8788881b617a03e145f
Keroldias123 <keroldias20@gmail.com>
2026-01-10 13:09:02 +0000

    model user

---
+++ packages/auth/models/user.ts
@@ -0,0 +1,11 @@
+// usuário (usuário)
+import { z } from 'zod'
+
+import { roleSchema } from '../role.ts'
+
+export const userSchema = z.object({
+  id: z.string(),
+  role: roleSchema,
+})
+
+export type User = z.infer<typeof userSchema>
